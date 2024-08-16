5841c7dd018f45b3da86cc3393930c9a6d01460f
Bhavesh <Bhavesh@DESKTOP-S6ETAE7>
2024-08-16 23:00:53 +0530

    commit

---
+++ BootMVCProj02_DataRendring/src/main/java/com/bsm/BootMvcProj02Application.java
@@ -0,0 +1,11 @@
+package com.bsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj02Application {
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj02Application.class, args);
+	}
+}
---
+++ BootMVCProj02_DataRendring/src/main/java/com/bsm/controll/ShowHomeController.java
@@ -0,0 +1,32 @@
+package com.bsm.controll;
+
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class ShowHomeController {
+	/*
+	 * @GetMapping("/process") public String process() {
+	 * System.out.println("ShowHomeController.process()"); return "forward:report";
+	 * }
+	 * 
+	 * @GetMapping("/report") public String showReport(Map<String,Object> map) {
+	 * System.out.println("ShowHomeController.showReport()"); return "show_data"; }
+	 */
+
+	/*
+	 * @RequestMapping("/process") public String process(HttpServletRequest req) {
+	 * System.out.println("ShowHomeController.process()"+req.hashCode()); return
+	 * "redirect:report"; }
+	 * 
+	 * @RequestMapping("/report") public String showReport(HttpServletRequest req) {
+	 * System.out.println("ShowHomeController.showReport()"+req.getAttribute("attr1"
+	 * )); return "show_data"; }
+	 */
+	
+	 
+}
